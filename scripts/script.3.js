ebc154d285298ea60089f1c05d38a6f4c77ed7a6
Duda <dudagcamargos037@gmail.com>
2024-10-01 14:03:27 -0300

    Update script.3.js

--- scripts/script.3.js
+++ scripts/script.3.js
@@ -4,7 +4,7 @@ let numero;
 for (;;) {
     numero = parseFloat(prompt("Digite um número (0 para sair):"));
     if (numero === 0) {
-        break; // Sai do laço quando o número for 0
+        break; 
     }
     if (numero > 0) {
         somatorio += numero;
